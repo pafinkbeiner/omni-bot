9ee32910be57bf411f7e214f51ec8b6799df79ca
pafinkbeiner <paul-d-f@web.de>
2023-07-04 19:06:51 +0200

    changed from rust -> go because of pwm channel limitation

---
+++ main.go
@@ -0,0 +1,27 @@
+package omnibot
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/stianeikeland/go-rpio/v4"
+)
+
+func main() {
+	fmt.Println("Starting Program...")
+
+	err := rpio.Open()
+	if err != nil {
+		os.Exit(1)
+	}
+
+	defer rpio.Close()
+
+	pin := rpio.Pin(19)
+	pin.Mode(rpio.Pwm)
+	pin.Freq(50)
+	pin.DutyCycle(0, 32)
+
+	time.Sleep(time.Second * 3)
+}
